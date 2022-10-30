8552f08fbf41601043b5c86dfb269d3a1fbe2677
kiyo-yuuma <kiyokawa0702@outlook.jp>
2022-10-30 14:25:47 +0900

    課題1-2作成

---
+++ prmn2022a/src/lecture01/MyInfo2.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+// 課題1-2
+public class MyInfo2 {
+
+    public static void main(String[] args) {
+        int studentNumber = 2210840;
+        System.out.println("B" + studentNumber + " Yuuma Kiyokawa");
+    }
+}
