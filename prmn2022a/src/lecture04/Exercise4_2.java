4ff08f040b36abd060ead3a8e016d34e8198be7b
kiyo-yuuma <kiyokawa0702@outlook.jp>
2022-11-24 11:19:47 +0900

    mainメソッド内容追加

--- prmn2022a/src/lecture04/Exercise4_2.java
+++ prmn2022a/src/lecture04/Exercise4_2.java
@@ -5,20 +5,35 @@ public class Exercise4_2 {
     public static void main(String[] args) {
         ATM atm = new ATM();
 
-        String name = "〇〇";
-        String number = "12345";
+        String name1 = "〇〇";
+        String number1 = "12345";
 
-        atm.registerAccount(name, number);
-        atm.registerAccount(name, number);
+        atm.registerAccount(name1, number1);
+        atm.registerAccount(name1, number1);
+
+        String name2 = "××";
+        String number2 = "67890";
+
+        atm.registerAccount(name2, number2);
+        atm.registerAccount(name2, number2);
 
         long money = 1000;
-        atm.deposit(number, money);
+        atm.deposit(number1, money);
+
+        money = 3000;
+        atm.deposit(number2, money);
+
+        // 存在しないナンバー
+        atm.deposit("13579", money);
 
         money = 2000;
-        atm.withdraw(number, money);
+        atm.withdraw(number1, money);
+
+        money = 2000;
+        atm.withdraw(number2, money);
 
         money = 500;
-        atm.withdraw(number, money);
+        atm.withdraw(number1, money);
 
     }
 }
